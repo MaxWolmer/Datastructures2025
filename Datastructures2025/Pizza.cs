a525fd311744a9c729ff1e530ac0034a3e8aabfb
Poul Henriksen <pohe@zealand.dk>
2025-09-24 10:37:24 +0200

    we

---
+++ Datastructures2025/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructures2025
+{
+    public class Pizza
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+
+        public Pizza(int number, string name, string description, double price)
+        {
+            Number = number;
+            Name = name;
+            Description = description;
+            Price = price; 
+        }
+
+        public override string ToString()
+        {
+            return $"{Number} {Name} {Description} {Price} kr";
+        }
+    }
+}
--- Datastructures2025/Program.cs
+++ Datastructures2025/Program.cs
@@ -1,42 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Datastructures2025;
+
 Console.WriteLine("Hello, World!");
 
-int[] myArray = new int[5];
-myArray[0] = 12;
-//myArray[1] = 3; 
-myArray[2] = 5;
-myArray[3] = 99;
-myArray[4] = 77;
+//int[] myArray = new int[5];
+//myArray[0] = 12;
+////myArray[1] = 3; 
+//myArray[2] = 5;
+//myArray[3] = 99;
+//myArray[4] = 77;
 
-int i1 = 12;
-int i2 = 3;
-int i3 = 4; 
+//int i1 = 12;
+//int i2 = 3;
+//int i3 = 4; 
 
-Console.WriteLine($"Det der er på index 3 {myArray[3]}");
+//Console.WriteLine($"Det der er på index 3 {myArray[3]}");
 
-myArray[4] = myArray[3] + 25;
+//myArray[4] = myArray[3] + 25;
 
-Console.WriteLine($"Det der er på index 4 {myArray[4]}");
+//Console.WriteLine($"Det der er på index 4 {myArray[4]}");
 
-for(int i = 0; i<5; i++)
-{
-    Console.WriteLine($"Indeks {i}  indeholder {myArray[i]} ");
-}
-Console.WriteLine("Foreach");
-foreach(int element in myArray)
-{
-    Console.WriteLine(element);
-}
+//for(int i = 0; i<5; i++)
+//{
+//    Console.WriteLine($"Indeks {i}  indeholder {myArray[i]} ");
+//}
+//Console.WriteLine("Foreach");
+//foreach(int element in myArray)
+//{
+//    Console.WriteLine(element);
+//}
 
-string[] names = new string[20];
-for(int i = 0; i<names.Length; i++)
-{
-    names[i] = "Default name";
-}
-names[0] = "Peter";
-names[1] = "Dorthe";
+//string[] names = new string[20];
+//for(int i = 0; i<names.Length; i++)
+//{
+//    names[i] = "Default name";
+//}
+//names[0] = "Peter";
+//names[1] = "Dorthe";
 
-Console.WriteLine(names[1][2]);
+//Console.WriteLine(names[1][2]);
 
 //myArray[5] = 12;
 //Ulemper:
@@ -46,33 +48,63 @@ Console.WriteLine(names[1][2]);
 
 
 //List<int> myFirstList = new List<int>();
-List<int> myFirstList = [34, -233, 9801, 67, 2, -9582, 770];
+//List<int> myFirstList = [34, -233, 9801, 67, 2, -9582, 770];
 
-//myFirstList.Add(982);
+////myFirstList.Add(982);
 
-myFirstList.Insert(1, 230);
-myFirstList.Add(120);
+//myFirstList.Insert(1, 230);
+//myFirstList.Add(120);
+//myFirstList.Remove(67);
+//myFirstList.RemoveAt(3);
+//myFirstList[4] = 100;
 
-myFirstList[4] = 100;
-//foreach(int element in myFirstList)
+//foreach (int element in myFirstList)
 //{
 //    Console.WriteLine(element);
 //}
 
 
-for(int index =0; index < myFirstList.Count; index++ )
+//for (int index =0; index < myFirstList.Count; index++ )
+//{
+//    Console.WriteLine($"Indeks {index}  indeholder {myFirstList[index]} ");
+//}
+
+//Console.WriteLine("Listen skrevet ud bagfra");
+//for(int index = myFirstList.Count-1; index >= 0 ; index= index -1 )
+//{
+//    Console.WriteLine($"Indeks {index}  indeholder {myFirstList[index]} ");
+//}
+
+
+//myFirstList.Sort();
+//Console.WriteLine("After sort");
+
+//for (int index = 0; index < myFirstList.Count; index++)
+//{
+//    Console.WriteLine($"Indeks {index}  indeholder {myFirstList[index]} ");
+//}
+
+////List<string> namesList;
+////namesList =  new List<string>();
+////namesList.Add("Peter");
+///
+Pizza p1 = new Pizza(1, "Magheritha", "Tomat og ost", 135);
+Pizza p2 = new Pizza(2, "Vesuvio", "Tomat, ost, skinke", 120);
+List<Pizza> pizzas;
+pizzas = new List<Pizza>();
+pizzas.Add(p1);
+pizzas.Add(p2);
+
+pizzas.Add(new Pizza(3, "Hawaii", "Tomas, os, Ananas og skinke", 159));
+
+foreach(Pizza pizzaElement in pizzas )
 {
-    Console.WriteLine($"Indeks {index}  indeholder {myFirstList[index]} ");
+    Console.WriteLine($"Number {pizzaElement.Number} name {pizzaElement.Name}");
 }
-
-myFirstList.Sort();
-Console.WriteLine("After sort");
-
-for (int index = 0; index < myFirstList.Count; index++)
+double  sum = 0; 
+foreach(Pizza p in pizzas)
 {
-    Console.WriteLine($"Indeks {index}  indeholder {myFirstList[index]} ");
+    sum = sum + p.Price;
 }
+Console.WriteLine($"Den total sum er { sum}");
 
-//List<string> namesList;
-//namesList =  new List<string>();
-//namesList.Add("Peter");
